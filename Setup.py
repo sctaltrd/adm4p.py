517868e51840d176a72c42b272049d60e3af77e2
Vadim Katsman <vadimk@sctalt.com>
2023-03-21 14:13:23 -0500

    Setup.py

---
+++ Setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+packages = []
+packages.append("adm4p")
+setup(name="adm4p",
+      version="100.2023.0310",
+      packages=packages,
+      package_dir={"adm4p": "../adm4p"},
+      include_package_data=True,
+      zip_safe=False
+      python_requires='>=3.10',
+      )
