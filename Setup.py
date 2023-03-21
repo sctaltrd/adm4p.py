461a597e3a01aac1823b342282c30d39dcb5c82e
Vadim Katsman <56979236+vadimkatsman@users.noreply.github.com>
2023-03-21 14:19:58 -0500

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -4,7 +4,7 @@ packages.append("adm4p")
 setup(name="adm4p",
       version="100.2023.0310",
       packages=packages,
-      package_dir={"adm4p": "../adm4p"},
+      package_dir={"adm4p": "."},
       include_package_data=True,
       python_requires='>=3.10',
       )
